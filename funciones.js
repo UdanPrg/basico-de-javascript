99154e5f5a6c2ac8929681bc341e7c685a1dd2ec
Uriel Arana <udan1718@gmail.com>
2022-03-09 22:06:55 -0600

    Learn how functions Works

---
+++ funciones.js
@@ -0,0 +1,12 @@
+// Funciones Declarativas
+function miFuncion(){
+    return 3;
+}
+miFuncion();
+
+// Funciones de Expresión o anónima
+var miFuncion = function(a, b){
+    return a + b;
+}
+
+miFuncion();
