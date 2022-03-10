536d1e1e587c5d7a42f355ba0e4b3e820ff1de58
Uriel Arana <udan1718@gmail.com>
2022-03-09 22:08:29 -0600

    What are a varibles

---
+++ variables.js
@@ -0,0 +1,25 @@
+var nombre = "Uriel";
+
+// -- Estados de la variable
+
+// Declarar
+var edad;
+
+//-/ edad = undefined
+edad
+
+// Inicializar
+edad = 30;
+
+//-/ edad = 30
+edad
+
+// Declarar e inicializar variable
+var elementos = ["Computadora", "Celular"]; 
+
+var persona = {
+    nombre: "Uriel",
+    edad: 30
+}
+
+persona;
