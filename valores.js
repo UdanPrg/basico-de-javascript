24da420b5cf6da1abbe2a92e51f6ac4bb269bc32
Uriel Arana <udan1718@gmail.com>
2022-03-09 22:08:09 -0600

    Types Values

---
+++ valores.js
@@ -0,0 +1,19 @@
+// -- Valores Primitivos
+
+// Number
+40
+// String
+"Uriel Arana"
+// Boolean
+true
+false
+// Empty values
+null
+undefined
+
+//-- Valores Tipo Objetos
+
+// Array
+[1,2,3]
+// Objeto
+{nombre: "Diego"}
